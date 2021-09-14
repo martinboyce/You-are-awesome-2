5ba99623d8b31e2724d9a06094c7438cd3db8511
Martin Boyce <martinboyce@umass.edu>
2021-09-14 13:18:18 -0400

    Initial Commit

---
+++ You are awesome 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You are awesome 2
+//
+//  Created by Martin Boyce on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
